aed59d2c9f4339423149a67cb5f9654c2dc2317e
Piotr <pzdziarski01@gmail.com>
2018-09-18 23:58:24 +0200

    getting productss from api

--- app/Http/Controllers/SitesController.php
+++ app/Http/Controllers/SitesController.php
@@ -3,20 +3,45 @@
 namespace App\Http\Controllers;
 
 
+use App\Dish;
+use App\Http\Resources\DishResource;
+
 class SitesController extends Controller
 {
+    /**
+     * menu subpage
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function menu()
     {
         return view('menu');
     }
 
+
+    /**
+     * reservation subaoge
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function reservations()
     {
         return view('reservation');
     }
 
+
+    /**
+     * order online subpage
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function order()
     {
-        return view('order');
+        $dishes = Dish::take(15)->get();
+        $dishes = collect($dishes);
+        $dishesCount = count($dishes);
+
+
+        return view('order', [
+            'dishes' => $dishes,
+            'dishesCount' => $dishesCount
+        ]);
     }
 }
